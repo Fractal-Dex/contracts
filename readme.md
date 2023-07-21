f6a3cdce2bce55b489acb59f8f32ac708f4d31c1
fractaldev <dev@fractal.dev>
2023-07-21 14:49:25 -0300

    execut pending tx, new cache for deployment

--- readme.md
+++ readme.md
@@ -13,9 +13,9 @@ Version: 0.8.13
 
 # Contracts:
 
-- [contracts/Token.sol](contracts/Token.sol) [`0x0fCfB023c73407271D87BC6d80B0440aD6eDB9F9`](https://opbnbscan.com/address/0x0fCfB023c73407271D87BC6d80B0440aD6eDB9F9) ([abi](abi/Token.json))
-- [contracts/PairFactory.sol](contracts/PairFactory.sol) [`0xc8188C2AfDFd990293d938092567bE8ed9E78Ec9`](https://opbnbscan.com/address/0xc8188C2AfDFd990293d938092567bE8ed9E78Ec9) ([abi](abi/PairFactory.json))
-- [contracts/Router.sol](contracts/Router.sol) [`0x424c033ab0b79F183B4e0189B20F9eEeeBD19350`](https://opbnbscan.com/address/0x424c033ab0b79F183B4e0189B20F9eEeeBD19350) ([abi](abi/Router.json))
+- [contracts/Token.sol](contracts/Token.sol) [`0x1D7307c0F283Fd5F98fE84290cA7f3433cc94035`](https://opbnbscan.com/address/0x1D7307c0F283Fd5F98fE84290cA7f3433cc94035) ([abi](abi/Token.json))
+- [contracts/PairFactory.sol](contracts/PairFactory.sol) [`0x09a9C738dB0BA3E47D21d43d23de606666009066`](https://opbnbscan.com/address/0x09a9C738dB0BA3E47D21d43d23de606666009066) ([abi](abi/PairFactory.json))
+- [contracts/Router.sol](contracts/Router.sol) [`0x008b72621420DaE6869E6df9fC61466ecbAC973e`](https://opbnbscan.com/address/0x008b72621420DaE6869E6df9fC61466ecbAC973e) ([abi](abi/Router.json))
 - [contracts/Router2.sol](contracts/Router2.sol) [`0x7D58DA6719e575e9EC8e50E249352554829aA905`](https://opbnbscan.com/address/0x7D58DA6719e575e9EC8e50E249352554829aA905) ([abi](abi/Router2.json))
 - [contracts/GaugeFactory.sol](contracts/GaugeFactory.sol) [`0x11Eb992C1deAec6028c8eebB6C3e0aDa55B57741`](https://opbnbscan.com/address/0x11Eb992C1deAec6028c8eebB6C3e0aDa55B57741) ([abi](abi/GaugeFactory.json))
 - [contracts/BribeFactory.sol](contracts/BribeFactory.sol) [`0xcD3b4d44e353C28e5A7Cd6E64bD21e51426D0031`](https://opbnbscan.com/address/0xcD3b4d44e353C28e5A7Cd6E64bD21e51426D0031) ([abi](abi/BribeFactory.json))
@@ -30,12 +30,16 @@ Version: 0.8.13
 - [contracts/MerkleClaim.sol](contracts/MerkleClaim.sol) [`0x5b4DE9c41c08DDe6cbcACceA12FB2fd899C39184`](https://opbnbscan.com/address/0x5b4DE9c41c08DDe6cbcACceA12FB2fd899C39184) ([abi](abi/MerkleClaim.json))
 # Transactions:
 
- - token.initialMint: [`0xfdde`](https://opbnbscan.com/tx/0xfdded0515deb653df46904ac7404aca1accacdeb252066c8bacd1bba9fe81030)
- - token.setMinter: [`0x266c`](https://opbnbscan.com/tx/0x266c124a39da9f570e4d759252ad2a7fd5308003ef31e47b4f78d4baf040de93)
- - pairFactory.setPauser: [`0xbee2`](https://opbnbscan.com/tx/0xbee2a712e2af833a56c75fb1df1d30e168f3ef43def65b6b17c1f97222971246)
- - escrow.setVoter: [`0x4871`](https://opbnbscan.com/tx/0x48712c98690108f84051d05afacc0477a3f2fc5af758493996f9d60f95d3c8d7)
- - voter.setGovernor: [`0x558c`](https://opbnbscan.com/tx/0x558cf8468136ea0d6b69c47fda843a9b49dd3e7e2312742b8dc45b529aec7755)
- - distributor.setDepositor: [`0xd37b`](https://opbnbscan.com/tx/0xd37b70ecd91c383ddbf580735e01b5e1f9ca9d3cf10616a5f4e31628f8405698)
- - voter.initialize: [`0xf7a7`](https://opbnbscan.com/tx/0xf7a727b73669496309ad20f55d07bd1c16169197933e96c44b5e1f541c2713aa)
- - minter.initialize: [`0x0c21`](https://opbnbscan.com/tx/0x0c21afd7ed519bf7f0cfcdc56d93ddfabba8c7352b06e4a1ca4045a90be67502)
- - minter.setTeam: [`0xcca9`](https://opbnbscan.com/tx/0xcca97e676a17ffea4aba2eda46346fd75cb78ab3518bde539ae9f1b677724374)
+ - Token.initialMint: [`0x0f4f`](https://opbnbscan.com/tx/0x0f4f778bc6fb1571b9812ecb592ec3256de3810b10b8732df203b76ac67e77d3)
+ - Token.setMerkleClaim: [`0x09d3`](https://opbnbscan.com/tx/0x09d3f6666cdd0844cc70ea9a737f372709fc2db286fab929d6d339accfba6cbe)
+ - Token.setMinter: [`0x3426`](https://opbnbscan.com/tx/0x34262440839011cfcadfac38f691b462c30c24b073df5a44ae562d8f8e8e774a)
+ - PairFactory.setPauser: [`0xfa79`](https://opbnbscan.com/tx/0xfa7979b04762ad6309d6e38c10a89ecd6d756641eeb79f09e9e312eb0bdef190)
+ - VotingEscrow.setVoter: `error`
+ - VotingEscrow.setTeam: [`0x9961`](https://opbnbscan.com/tx/0x9961df9e181a59b85433945afe3b518c8067ebead135d3cbdeb8651ba3932132)
+ - Voter.setGovernor: [`0xe747`](https://opbnbscan.com/tx/0xe74709d6d6cee85d82a6487f14c82f349f701ee16e1895e0b5a50d9fe9b9a243)
+ - Voter.setEmergencyCouncil: [`0x9472`](https://opbnbscan.com/tx/0x9472054ecf7506bf85653962bcd4377b766ced96d1138a89a4bc4d8ac6ddd694)
+ - RewardsDistributor.setDepositor: `error`
+ - TokenGovernor.setTeam: [`0x3730`](https://opbnbscan.com/tx/0x37304b9725b6161bcbd4de422f0108770e228c20aa60b01d78d240ab8aeb8fc5)
+ - Voter.initialize: `error`
+ - Minter.initialize: `error`
+ - Minter.setTeam: [`0xe277`](https://opbnbscan.com/tx/0xe27737912bfcd524cd307150a368067bfd9b9ef47fa7e9abcec3c38793a739da)
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,80 +1,67 @@
 import getDeploymentConfig from "../deployment-config";
 import fs from "fs";
 import {ethers} from "ethers";
-let chainId: number, mainNet: boolean, testNet: boolean;
+let chainId: number, mainNet: boolean, testNet: boolean, isHardhat: boolean;
 let CONTRACTS: any = {};
 let CONFIG = {};
-let tx; // running tx variable to wait for execution
 let deployer:hre.ethers.Wallet; // signer for tx
-function get(name: string): string {
-    if (CONTRACTS[name])
-        return CONTRACTS[name];
-    const cache = `contracts-${chainId}.json`;
-    if (!fs.existsSync(cache))
-        fs.writeFileSync(cache, "{}");
-    const contracts = fs.readFileSync(cache, "utf8");
-    // @ts-ignore
-    const addr = contracts[name];
-    CONTRACTS[name] = addr;
-    return addr;
+
+let nonceOffset = 0;
+async function getNonce():Promise<number>{
+    return await hre.ethers.provider.getTransactionCount(deployer.address, "latest");
+}
+function fromWei(wei:bigint):string{
+    return ethers.utils.formatEther(wei);
 }
 
-function set(name: string, addr: string): string {
-    const cache = `contracts-${chainId}.json`;
-    if (!fs.existsSync(cache))
-        fs.writeFileSync(cache, "{}");
-    const contracts = fs.readFileSync(cache, "utf8");
-    const json = JSON.parse(contracts);
-    json[name] = addr;
-    CONTRACTS[name] = addr;
-    fs.writeFileSync(cache, JSON.stringify(json, null, 4));
-    //console.log(`# ${name} deployed at ${addr}`);
-    return addr;
-}
-
-async function verify(contract: any, args: any[] = []) {
-
-    return true;
-
-    // dump balance of deployer:
-    const balance = await hre.ethers.provider.getBalance(deployer.address);
-    console.log(` - Balance of deployer: ${ethers.utils.formatEther(balance)}`);
-    const addr = contract.address;
-    const verifyArgs = {
-        address: addr,
-        constructorArguments: args,
-    };
-    try {
-        if ( mainNet || testNet) {
-            await contract.deployTransaction.wait(5);
-            hre.run("verify:verify", verifyArgs);
-        }
-    } catch (e) {
-        console.log(` - Verification failed for ${addr}`);
-        console.log(` - verifyArgs:`, verifyArgs);
-        console.log(e);
-    }
-}
-
-async function flatten(name:string, files:string[]) {
-    let flattened = '';
-    try {
-        flattened = await hre.run("flatten:get-flattened-sources", {files});
-        flattened = flattened.replaceAll(/SPDX-License-Identifier:/gm, "License-Identifier:");
-        flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
-        flattened = flattened.replace(/pragma experimental ABIEncoderV2;\n/gm, ((i) => (m) => (!i++ ? m : ""))(0));
-    } catch (e) {
-        console.log(` - Flattening failed for ${name}`);
-        console.log(e);
+async function tx(contract:ethers.Contract, method:string, args:any[] = []){
+    const buildInfo = contract.buildInfo;
+    if( ! buildInfo ) console.log('contract.buildInfo not found');
+    const name = `${buildInfo.contractName}.${method}`;
+    let hash = get(name);
+    if(hash) {
+        console.log(` - tx: ${name} = ${hash} (skipped)`);
         return;
     }
-    fs.writeFileSync(`flattened/${name}.sol`, flattened);
+    try{
+        const fctlEstimate = contract.estimateGas[method];
+        if( ! fctlEstimate ) throw new Error(` - method ${method} not found`);
+        const estimatedGasLimit = await fctlEstimate(...args);
+        const txUnsigned = await contract.populateTransaction[method](...args);
+        txUnsigned.chainId = chainId;
+        txUnsigned.gasLimit = estimatedGasLimit.mul(110).div(100);
+        const gasPrice = await hre.ethers.provider.getGasPrice();
+        txUnsigned.gasPrice = gasPrice.mul(110).div(100);
+        txUnsigned.nonce = await getNonce();
+        const txCostInWei = txUnsigned.gasLimit.mul(txUnsigned.gasPrice);
+        const balance = await hre.ethers.provider.getBalance(deployer.address);
+        if( balance.lt(txCostInWei) ) throw new Error(` - Insufficient balance: ${ethers.utils.formatEther(balance)} < ${ethers.utils.formatEther(txCostInWei)}`);
+        console.log(` - tx: ${name} (COST=${fromWei(txCostInWei)}, gas=${txUnsigned.gasLimit}, gasPrice=${fromWei(txUnsigned.gasPrice)})`);
+        const txSigned = await deployer.signTransaction(txUnsigned);
+        const submittedTx = await hre.ethers.provider.sendTransaction(txSigned);
+        const receipt = await submittedTx.wait();
+        if (receipt.status === 0) throw new Error(" - Approve transaction failed");
+        let hash = submittedTx.hash;
+        const alias = hash.substring(0, 6);
+        const line = ` - ${name}: [\`${alias}\`](${CONFIG.EXPLORER}tx/${hash})\n`;
+        README += line;
+        set(name, hash);
+        console.log(`       ${hash}`);
+    }catch(e) {
+        const err = e.toString();
+        const line = ` - ${name}: \`error\`\n`;
+        nonceOffset--;
+        README += line;
+        console.log(`${name}: ${err}`);
+        console.log(e);
+    }
 }
 
 let README = "";
-async function deployOrLoad(name: string, factory: any, args: any[] = []) {
+async function deployOrLoad(name: string, factory: any, args: any[] = []): Promise<ethers.Contract> {
     let addr = get(name);
     if (!addr) {
+        new Error(`Address of ${name} not found`);
         const contract = await factory.deploy(...args);
         await contract.deployed();
         addr = contract.address;
@@ -94,7 +81,9 @@ async function deployOrLoad(name: string, factory: any, args: any[] = []) {
     // encode arguments in abi format:
     const encoded = constructorArgs(abi, args);
     fs.writeFileSync(argumentsFile, encoded.replace(/0x/, ""));
-    return await hre.ethers.getContractAt(name, addr);
+    let ctx = await hre.ethers.getContractAt(name, addr, deployer);
+    ctx.buildInfo =buildInfo;
+    return ctx;
 }
 
 function constructorArgs(abi: any, args: any[] = []): string {
@@ -114,9 +103,16 @@ async function main() {
     chainId = network.chainId;
     mainNet = chainId === 5610;
     testNet = chainId === 5611;
+    isHardhat = chainId === 31337;
+
+    let privateKey = process.env.PRIVATE_KEY;
+    if( isHardhat ) {
+        // PRIVATE_KEY_HARHDAT
+        privateKey = process.env.PRIVATE_KEY_HARHDAT
+    }
     CONFIG = getDeploymentConfig(mainNet);
     // public key from private key:
-    deployer = new hre.ethers.Wallet(process.env.PRIVATE_KEY);
+    deployer = new hre.ethers.Wallet(privateKey);
     console.log(`#Network: ${chainId}, deployer: ${deployer.address}`);
 
     README = `# Network: ${hre.network.name} #${chainId}\n\n`;
@@ -160,56 +156,115 @@ async function main() {
         hre.ethers.getContractFactory("MerkleClaim"),
         hre.ethers.getContractFactory("WrappedExternalBribeFactory"),
     ]);
-    const token = await deployOrLoad("Token", Token);
-    const pairFactory = await deployOrLoad("PairFactory", PairFactory);
-    const router = await deployOrLoad("Router", Router, [pairFactory.address, CONFIG.WETH]);
-    const router2 = await deployOrLoad("Router2", Router2, [pairFactory.address, CONFIG.WETH]);
-    const gaugeFactory = await deployOrLoad("GaugeFactory", GaugeFactory);
-    const bribeFactory = await deployOrLoad("BribeFactory", BribeFactory);
-    const library = await deployOrLoad("TokenLibrary", Library, [router2.address]);
-    const artProxy = await deployOrLoad("VeArtProxy", VeArtProxy);
-    const escrow = await deployOrLoad("VotingEscrow", VotingEscrow, [token.address, artProxy.address]);
-    const distributor = await deployOrLoad("RewardsDistributor", RewardsDistributor, [escrow.address]);
-    const voter = await deployOrLoad("Voter", Voter, [escrow.address, pairFactory.address, gaugeFactory.address, bribeFactory.address]);
-    const externalBribeFactory = await deployOrLoad("WrappedExternalBribeFactory", WrappedExternalBribeFactory, [voter.address]);
-    const minter = await deployOrLoad("Minter", Minter, [voter.address, escrow.address, distributor.address]);
-    const governor = await deployOrLoad("TokenGovernor", TokenGovernor, [escrow.address]);
-    const claim = await deployOrLoad("MerkleClaim", MerkleClaim, [token.address, CONFIG.merkleRoot]);
+    const token:ethers.Contract = await deployOrLoad("Token", Token);
+
+    const pairFactory:ethers.Contract = await deployOrLoad("PairFactory", PairFactory);
+    const router:ethers.Contract = await deployOrLoad("Router", Router, [pairFactory.address, CONFIG.WETH]);
+    const router2:ethers.Contract = await deployOrLoad("Router2", Router2, [pairFactory.address, CONFIG.WETH]);
+    const gaugeFactory:ethers.Contract = await deployOrLoad("GaugeFactory", GaugeFactory);
+    const bribeFactory:ethers.Contract = await deployOrLoad("BribeFactory", BribeFactory);
+    const library:ethers.Contract = await deployOrLoad("TokenLibrary", Library, [router2.address]);
+    const artProxy:ethers.Contract = await deployOrLoad("VeArtProxy", VeArtProxy);
+    const escrow:ethers.Contract = await deployOrLoad("VotingEscrow", VotingEscrow, [token.address, artProxy.address]);
+    const distributor:ethers.Contract = await deployOrLoad("RewardsDistributor", RewardsDistributor, [escrow.address]);
+    const voter:ethers.Contract = await deployOrLoad("Voter", Voter, [escrow.address, pairFactory.address, gaugeFactory.address, bribeFactory.address]);
+    const externalBribeFactory:ethers.Contract = await deployOrLoad("WrappedExternalBribeFactory", WrappedExternalBribeFactory, [voter.address]);
+    const minter:ethers.Contract = await deployOrLoad("Minter", Minter, [voter.address, escrow.address, distributor.address]);
+    const governor:ethers.Contract = await deployOrLoad("TokenGovernor", TokenGovernor, [escrow.address]);
+    const claim:ethers.Contract = await deployOrLoad("MerkleClaim", MerkleClaim, [token.address, CONFIG.merkleRoot]);
 
     README += `# Transactions:\n\n`;
-    await runTx("token.initialMint", token.initialMint(CONFIG.teamTreasure, CONFIG.teamAmount))
-    await runTx("token.setMerkleClaim", token.setMerkleClaim(claim.address))
-    await runTx("token.setMinter", token.setMinter(minter.address))
-    await runTx("pairFactory.setPauser", pairFactory.setPauser(CONFIG.teamEOA))
-    await runTx("escrow.setVoter", escrow.setVoter(voter.address))
-    await runTx("escrow.setTeam", escrow.setTeam(CONFIG.teamEOA))
-    await runTx("voter.setGovernor", voter.setGovernor(CONFIG.teamEOA))
-    await runTx("voter.setEmergencyCouncil", voter.setEmergencyCouncil(CONFIG.teamEOA))
-    await runTx("distributor.setDepositor", distributor.setDepositor(minter.address))
-    await runTx("governor.setTeam", governor.setTeam(CONFIG.teamEOA))
+    await tx(token, "initialMint", [CONFIG.teamTreasure, CONFIG.teamAmount] )
+    await tx(token, "initialMint", [CONFIG.teamTreasure, CONFIG.teamAmount] )
+
+
+    await tx(token, "setMerkleClaim", [claim.address] )
+    await tx(token, "setMinter", [minter.address] )
+    await tx(pairFactory, "setPauser", [CONFIG.teamEOA] )
+    await tx(escrow, "setVoter", [voter.address] )
+    await tx(escrow, "setTeam", [CONFIG.teamEOA] )
+    await tx(voter, "setGovernor", [CONFIG.teamEOA] )
+    await tx(voter, "setEmergencyCouncil", [CONFIG.teamEOA] )
+    await tx(distributor, "setDepositor", [minter.address] )
+    await tx(governor, "setTeam", [CONFIG.teamEOA] )
     const nativeToken = [token.address]
     const tokenWhitelist = nativeToken.concat(CONFIG.tokenWhitelist)
-    await runTx("voter.initialize", voter.initialize(tokenWhitelist, minter.address))
+    await tx(voter, "initialize", [tokenWhitelist, minter.address] )
 
     let total:bigint = BigInt(0)
     for( let i in CONFIG.partnerAmts )
         total += BigInt(CONFIG.partnerAmts[i])
-    await runTx("minter.initialize", minter.initialize(CONFIG.partnerAddrs, CONFIG.partnerAmts, total))
-    await runTx("minter.setTeam", minter.setTeam(CONFIG.teamMultisig))
+    await tx(minter, "initialize", [CONFIG.partnerAddrs, CONFIG.partnerAmts, total] )
+    await tx(minter, "setTeam", [CONFIG.teamMultisig])
 
     fs.writeFileSync("readme.md", README);
 
 }
 
-async function runTx(name, transaction:Promise<any>){
-    try{
-        const tx = await transaction;
-        tx.wait();
-        const alias = tx.hash.substring(0, 6);
-        README += ` - ${name}: [\`${alias}\`](${CONFIG.EXPLORER}tx/${tx.hash})\n`;
-    }catch(e) {
+
+async function verify(contract: any, args: any[] = []) {
+
+    return true;
+
+    // dump balance of deployer:
+    const balance = await hre.ethers.provider.getBalance(deployer.address);
+    console.log(` - Balance of deployer: ${ethers.utils.formatEther(balance)}`);
+    const addr = contract.address;
+    const verifyArgs = {
+        address: addr,
+        constructorArguments: args,
+    };
+    try {
+        if ( mainNet || testNet) {
+            await contract.deployTransaction.wait(5);
+            hre.run("verify:verify", verifyArgs);
+        }
+    } catch (e) {
+        console.log(` - Verification failed for ${addr}`);
+        console.log(` - verifyArgs:`, verifyArgs);
         console.log(e);
     }
+}
+
+async function flatten(name:string, files:string[]) {
+    let flattened = '';
+    try {
+        flattened = await hre.run("flatten:get-flattened-sources", {files});
+        flattened = flattened.replaceAll(/SPDX-License-Identifier:/gm, "License-Identifier:");
+        flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
+        flattened = flattened.replace(/pragma experimental ABIEncoderV2;\n/gm, ((i) => (m) => (!i++ ? m : ""))(0));
+    } catch (e) {
+        console.log(` - Flattening failed for ${name}`);
+        console.log(e);
+        return;
+    }
+    fs.writeFileSync(`flattened/${name}.sol`, flattened);
+}
+
+function set(name: string, addr: string): string {
+    const cache = `contracts-${chainId}.json`;
+    if (!fs.existsSync(cache))
+        fs.writeFileSync(cache, "{}");
+    const contracts = fs.readFileSync(cache, "utf8");
+    const json = JSON.parse(contracts);
+    json[name] = addr;
+    CONTRACTS[name] = addr;
+    fs.writeFileSync(cache, JSON.stringify(json, null, 4));
+    //console.log(`# ${name} deployed at ${addr}`);
+    return addr;
+}
+
+function get(name: string): string {
+    if (CONTRACTS[name])
+        return CONTRACTS[name];
+    const cache = `contracts-${chainId}.json`;
+    if (!fs.existsSync(cache))
+        fs.writeFileSync(cache, "{}");
+    const contracts = JSON.parse( fs.readFileSync(cache, "utf8") );
+    // @ts-ignore
+    const addr = contracts[name];
+    CONTRACTS[name] = addr;
+    return addr;
 }
 
 main()
